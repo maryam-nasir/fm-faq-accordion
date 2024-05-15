5ca4e4ee034d9fce6064e23daf6fa64c336481dc
Maryam Nasir <maryamnasir555@yahoo.com>
2024-05-15 18:37:19 +0500

    Add app colors in theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "hsl(0, 0%, 100%)",
+        lightPink: "hsl(275, 100%, 97%)",
+        grayishPurple: "hsl(292, 16%, 49%)",
+        darkPurple: "hsl(292, 42%, 14%)",
+      },
+    },
   },
   plugins: [],
 };
